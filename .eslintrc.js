2fbb356bf5dbdbaf8d54f4df70be4f792fcc1f1e
Danil <75947542+Dandillo@users.noreply.github.com>
2023-05-23 19:30:12 +0500

    project init

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+﻿module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "unicode-bom": ["error", "never"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
