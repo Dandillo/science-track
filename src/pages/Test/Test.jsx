2d95e7e5e4f8f019615cd67dc4ef09cb38cb0943
Stepan <stepan.sharifulin@mail.ru>
2023-09-20 17:36:24 +0500

    Repair webpack problems

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,17 +1,11 @@
-<<<<<<< Updated upstream
 import React, { useState } from "react";
 import "./Test.scss";
 import image from "../../assets/images/MainPhoto.png";
 import ProgressCircle from "../../components/EventContainer/ProgressCircle";
-=======
-import React, { useState } from 'react'
-import './Test.scss';
->>>>>>> Stashed changes
 
 function Test() {
   const [testProgress, setTestProgress] = useState(0);
   return (
-<<<<<<< Updated upstream
     <div className="test">
       <input
         style={{ border: "1px solid black" }}
@@ -21,26 +15,6 @@ function Test() {
         onClick={(e) => setTestProgress(e.target.value)}
       />
       <ProgressCircle roundNumber={34} progress={testProgress} />
-=======
-    <div className='test z-0 flex items-center justify-center w-screen h-screen bg-white' >
-      {/* <div className='test__child w-[600px] h-[600px] flex items-center justify-center'>
-        <div className='w-[200px] h-[200px] bg-red-600 z-20'/>
-      </div> */}
-      <svg
-        xmlnsXlink='http://www.w3.org/1999/xlink'
-        width='300' height='300'>
-
-        <filter id='n' x='0' y='0'>
-                <feTurbulence
-                  type='fractalNoise'
-                  baseFrequency='0.75'
-                  stitchTiles='stitch'/>
-        </filter>
-
-        <rect width='300' height='300' fill='#fff'/>
-        <rect width='300' height='300' filter="url(#n)" opacity='0.60'/>
-      </svg>
->>>>>>> Stashed changes
     </div>
   );
 }
